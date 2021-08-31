49262e719fbd5874b82bc0d99bba846941c79369
Aleksandr <aswom@mail.ru>
2021-08-31 11:45:55 +0200

    morning_3108

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () { 
+$('.single-item').slick({
+      infinite: true,
+      dots: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+       autoplay: true,
+  autoplaySpeed: 2000
+});
+   });
