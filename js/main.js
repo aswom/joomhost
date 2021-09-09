8cad17af7fa6c2903c652464c38a6c29845f1902
Aleksandr <aswom@mail.ru>
2021-09-09 18:17:14 +0200

    new-0909

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ $('.single-item').slick({
        autoplay: true,
   autoplaySpeed: 2000
 });
+});
+   // jquery menu
+$(function () {
+   $('.burger__menu').on('click', function (e) {
+      e.preventDefault;
+      //$(this).toggleClass('burger__menu-active');
+      $(this).toggleClass('header__list-active');
    });
+
+});
+
+const burger = document.querySelector('.burger__menu');
+const headerList = document.querySelector('.headerList');
+
+burger.addEventListener("click", () => {
+   headerList.classList.toggle('active');
+});
