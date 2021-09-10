86fd4990e0f1e84f59d2b68a4c69e297ac84e2d8
Aleksandr <aswom@mail.ru>
2021-09-10 13:38:26 +0200

    last-adapt

--- js/main.js
+++ js/main.js
@@ -5,7 +5,16 @@ $('.single-item').slick({
       slidesToShow: 2,
       slidesToScroll: 1,
        autoplay: true,
-  autoplaySpeed: 2000
+   autoplaySpeed: 2000,
+  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }
+    ]
+
 });
 });
    // jquery menu
@@ -13,14 +22,17 @@ $(function () {
    $('.burger__menu').on('click', function (e) {
       e.preventDefault;
       //$(this).toggleClass('burger__menu-active');
-      $(this).toggleClass('header__list-active');
+      
    });
 
 });
 
-const burger = document.querySelector('.burger__menu');
+const burger = burger__menu = document.querySelector('.burger__menu');
+//const burger__menu = document.querySelector('.burger__menu');
 const headerList = document.querySelector('.headerList');
 
 burger.addEventListener("click", () => {
    headerList.classList.toggle('active');
+   burger__menu.classList.toggle('active');
+
 });
